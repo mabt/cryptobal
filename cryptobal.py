2b185748fb9b42c51f5def2009395071a208d276
Maxime <mbt@wegloo.org>
2018-11-20 14:59:29 +0100

    Update cryptobal.py

--- cryptobal.py
+++ cryptobal.py
@@ -8,23 +8,22 @@ import sys
 if __name__ == '__main__':
 
     input = (str(sys.argv[1]))
-    
+
     with open(input) as fic:
-        
-        print ("Name","\t","Price_usd","\t","Quantity","\t\t","Total")
-        
-        count = 0   
-        
+
+        print ("%6s %15s %15s %15s" % ("Name","Price_usd","Quantity","Total"))
+
+        count = 0
+
         for line in fic:
             api = "https://api.coinmarketcap.com/v1/ticker/"
             url = (api + line.split(':')[0])
             quantity = (line.split(':')[1])
             r = requests.get(url)
-            
+
             for coin in r.json():
                 price_usd = (coin["price_usd"])
                 count = (count) + (float(coin["price_usd"])*float(quantity))
-
-                print(coin["symbol"],"\t",price_usd,"\t",float(coin["price_usd"])*float(quantity),"\t",(float(coin["price_usd"])*float(quantity)))
-                
-        print ("\nTotal : ",(count),"$")
+                print ("%6s %15f %15f %15f" % (coin["symbol"],float(price_usd),float(coin["price_usd"])*float(quantity),(float(coin["price_usd"])*float(quantity))))
+        print ("\n")
+        print ("%38s %15f" % ("Total price : $",float(count)))
