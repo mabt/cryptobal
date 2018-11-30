5cc90789190840142fe4e8b6f9e4d4fc6054f590
Maxime <mbt@wegloo.org>
2018-11-30 14:18:20 +0100

    Update cryptobal.py

--- cryptobal.py
+++ cryptobal.py
@@ -10,9 +10,8 @@ if __name__ == '__main__':
     input = (str(sys.argv[1]))
 
     with open(input) as fic:
-
+        print ("\n")
         print ("%6s %15s %15s %15s" % ("Name","Price_usd","Quantity","Total"))
-
         count = 0
 
         for line in fic:
@@ -24,6 +23,7 @@ if __name__ == '__main__':
             for coin in r.json():
                 price_usd = (coin["price_usd"])
                 count = (count) + (float(coin["price_usd"])*float(quantity))
-                print ("%6s %15f %15f %15f" % (coin["symbol"],float(price_usd),float(coin["price_usd"])*float(quantity),(float(coin["price_usd"])*float(quantity))))
+                print ("%6s %15.3f %15f %15.2f" % (coin["symbol"],float(price_usd),float(quantity),(float(coin["price_usd"])*float(quantity))))
         print ("\n")
-        print ("%38s %15f" % ("Total price : $",float(count)))
+        print ("%38s %15.2f" % ("Total price : $",float(count)))
+        print ("\n")
